162f4fa27602ab981be87565aed7664b174cc8ba
Juliachu1213 <44692444+Juliachu1213@users.noreply.github.com>
2023-06-16 08:48:48 +0800

    Update schema.py

---
+++ EasyBookBackend/my-app/services/book/schema.py
@@ -0,0 +1,15 @@
+import re
+import datetime
+from pydantic.main import BaseModel
+from typing import Optional
+
+class BookRequest(BaseModel):
+    ID:int
+    RID: int
+    Name: str
+    Phone: str
+    Reason: str
+    ReTime:datetime.datetime 
+    Person: int  
+
+
