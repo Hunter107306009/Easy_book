c8b114e99664d81f579993a0574c2c684deec6e3
Juliachu1213 <44692444+Juliachu1213@users.noreply.github.com>
2023-06-16 17:38:00 +0800

    Update book.py

--- EasyBookBackend/my-app/routers/book.py
+++ EasyBookBackend/my-app/routers/book.py
@@ -3,11 +3,26 @@ from sqlalchemy.orm.session import Session
 from fastapi.params import Depends
 from utils.db_conn import get_db
 from services.book import business
+from services.book import business, schema
 
 
 router = APIRouter(prefix="/book", tags=["book"])
 
 
-@router.get("/")
-async def root():
-    return {"msg": 0}
+@router.post("/book",summary='訂位')
+async def book(
+    bookRequest : schema.BookRequest, db: Session = Depends(get_db)
+):
+    return await business.book(bookRequest, db)
+
+
+@router.delete("/cancel_book",summary='取消訂位')
+async def cancel_book(
+    cancelRequest : schema.CancelRequest, db: Session = Depends(get_db)
+):
+    return await business.cancel_book(cancelRequest, db)
+   
+
+
+
+
