598510dab0ec75b26afc482d2021ee989eeef0c0
Juliachu1213 <44692444+Juliachu1213@users.noreply.github.com>
2023-06-18 01:33:38 +0800

    Update schema.py

--- EasyBookBackend/my-app/services/book/schema.py
+++ EasyBookBackend/my-app/services/book/schema.py
@@ -24,6 +24,10 @@ class UpdateBookRequest(BaseModel):
 class QueryMemberRequest(BaseModel):
     MemberID: int
 
+class CheckSeatsRequest(BaseModel):
+    RID: int
+    BookTime: date
+    Person: int
 
 class QueryRestaurantRequest(BaseModel):
     RID: int
