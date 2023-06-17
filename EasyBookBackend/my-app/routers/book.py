f444b2c9e37e9f2d374a89fd0ea3b0d8c68328b7
Juliachu1213 <44692444+Juliachu1213@users.noreply.github.com>
2023-06-18 00:31:35 +0800

    Update book.py

--- EasyBookBackend/my-app/routers/book.py
+++ EasyBookBackend/my-app/routers/book.py
@@ -36,6 +36,6 @@ async def query_book_restaurant(RID: int, db: Session = Depends(get_db)):
     return await business.query_book_restaurant(RID, db)
     
 
-@router.get("/check_seats", summary="店家查詢座位資訊")
+@router.post("/check_seats", summary="店家查詢座位資訊")
 async def check_seats(RID: int,ReDate:str, Person:int,db: Session = Depends(get_db)):
     return await business.check_seats(RID,ReDate,Person,db)
